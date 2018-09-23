d651f55db2b9d0cb9e5b7187f169df3f3893c239
eduardojvr <eduardojvr10@gmail.com>
2018-09-23 14:17:10 -0300

    Implementation of the layout of screens

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+
+class Gerenciador(BoxLayout):
+    pass
+
+class TelaInicial(Screen):
+    pass
+    
+class TelaAfinacao(Screen):
+    pass
+    
+class Menu(Screen):
+	pass
+
+class Aplicacao(App):
+    def build(self):
+        return Gerenciador()
+
+if __name__ == "__main__":
+    Aplicacao().run()
