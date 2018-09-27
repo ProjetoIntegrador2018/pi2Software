573aebe507fe55d7a653f55cd0661969b2d862ab
IagoCarvalho <IagoVC_2012@hotmail.com>
2018-09-27 13:23:16 -0300

    Adding db slider initial configurations and assets test

--- src/telas.py
+++ src/telas.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.slider import Slider
 
 class Gerenciador(BoxLayout):
     pass
@@ -9,8 +10,15 @@ class TelaInicial(Screen):
     pass
     
 class TelaAfinacao(Screen):
-    pass
     
+    def show_db(self, *args):
+        self.ids.decibeis.text = str(int(args[1]))
+    
+    # Teste de comportamento
+    def test(self, *args):
+        if int(args[1]) == 50:
+            self.ids.barra_db.value = 0
+        
 class Menu(Screen):
     pass
 
