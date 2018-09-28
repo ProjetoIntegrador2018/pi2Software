8027d233058ce9e1c31068b7b88c42153d371e64
IagoCarvalho <IagoVC_2012@hotmail.com>
2018-09-27 21:35:52 -0300

    Adding mock to represent dbs

--- src/telas.py
+++ src/telas.py
@@ -1,7 +1,10 @@
+import random
+
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.slider import Slider
+from kivy.clock import Clock
 
 
 class Gerenciador(BoxLayout):
@@ -11,9 +14,12 @@ class TelaInicial(Screen):
     pass
     
 class TelaAfinacao(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Clock.schedule_interval(self.update, 1)
     
     def show_db(self, *args):
-        self.ids.decibeis.text = str(int(args[1]))
+        self.ids.decibeis.text = str(int(args[1])) + ' decibéis'
     
     def change_db_cursor(self, *args):
         if int(args[1]) < 0:
@@ -22,7 +28,19 @@ class TelaAfinacao(Screen):
             self.ids.barra_db.cursor_image = 'assets/cursor_vermelho.png'
         else:
             self.ids.barra_db.cursor_image = 'assets/cursor_verde.png'
-
+    
+    def mock_db_value(self):
+        while True:
+            self.ids.barra_db.value = random.randint(-50, 50)
+            return self.ids.barra_db.value
+    
+    def disable_touch_event(self):
+        return 0
+        
+    def update(self, *args):
+        self.ids.barra_db.value = self.mock_db_value()
+        
+        
         
 class Menu(Screen):
     pass
