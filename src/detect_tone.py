bc0b7a52ff19e9ab325c690dfaf403cb8e99bc62
ulysses <ulysses3353@gmail.com>
2018-10-24 17:11:13 -0300

    Fix note tuning identification and add tuning tests
    Co-authored-by: Iago <IagoVC_2012@hotmail.com>

--- src/detect_tone.py
+++ src/detect_tone.py
@@ -4,7 +4,42 @@ import time
 from math import log2, pow
 import store
 
+delta82 = -4
+delta110 = 3
+delta146 = -4 
+delta196 = 3
+delta246 = 10
+delta329 = 7
+
+def comput_frequency(audio_data):
+        frate = 44100.0
+        w = np.fft.fft(audio_data)
+        freqs = np.fft.fftfreq(len(w))
+        idx = np.argmax(np.abs(w))
+        freq = freqs[idx]
+        frequency = abs(freq * frate)
+
+        if(frequency > 85 and frequency < 87):
+               return frequency + delta82
+
+        if(frequency > 106 and frequency < 108):
+                return frequency + delta110
+
+        if(frequency > 149 and frequency < 151):
+                return frequency + delta146
+
+        if(frequency > 192 and frequency < 194):
+                return frequency + delta196
+
+        if(frequency > 235 and frequency < 237):
+                return frequency + delta246
         
+        if(frequency > 321 and frequency < 323):
+                return frequency + delta329
+
+
+        return frequency
+
 def get_tone():
         FORMAT = pyaudio.paInt16
         CHANNELS = 1
@@ -26,12 +61,7 @@ def get_tone():
 
         while(1):
                 audio_data = np.fromstring(stream.read(CHUNK), np.int16)
-                frate = 44100.0
-                w = np.fft.fft(audio_data)
-                freqs = np.fft.fftfreq(len(w))
-                idx = np.argmax(np.abs(w))
-                freq = freqs[idx]
-                frequency = abs(freq * frate)
+                frequency = comput_frequency(audio_data)
                 if(frequency > 0):
                         store.frequency = frequency   
 
