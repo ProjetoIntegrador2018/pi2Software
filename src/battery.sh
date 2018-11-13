db4a76e5e705075094bb947f556b6eca3b6035e7
eduardojvr <eduardojvr10@gmail.com>
2018-11-13 14:45:30 -0200

    Update battery

--- src/battery.sh
+++
@@ -1,5 +0,0 @@
-upower -i $(upower -e | grep BAT) | grep -E percentage | xargs | cut -d' ' -f2|sed s/%// > /tmp/.batteryPercentage
-upower -i $(upower -e | grep BAT) | grep -E state | xargs | cut -d' ' -f2|sed s/%// > /tmp/.batteryState
-cat /tmp/.batteryPercentage
-
-exit 0
