6844d6cbbd6f4c241b434e9646c8590506b85a9f
IagoCarvalho <IagoVC_2012@hotmail.com>
2018-11-28 01:21:01 +0000

    Fix frequency bar bug
    Co-authored-by: Eduardojvr <eduardojvr10@gmail.com>
    Co-authored-by: uly <ulysses3353@gmail.com>
    Co-authored-by: amandabezerra <amandabs_contato@hotmail.com>

--- src/screens.py
+++ src/screens.py
@@ -134,9 +134,12 @@ class TuningScreen(Screen):
 
     def frequency_range(self):
         cord_values = [329, 246, 196, 146, 110, 82, 404]
-        self.ids.db_bar.value = abs(int(store.frequency) - cord_values[self.cord])
+        self.ids.db_bar.value = int(store.frequency) - cord_values[self.cord]
+        aux = abs(int(store.frequency) - cord_values[self.cord])
+	        
+
         if(self.timestep < time.time()):
-            if(self.ids.db_bar.value <= 5  and self.cord != 6):
+            if(aux <= 1  and self.cord != 6):
                 self.cord = (self.cord + 1)
             self.timestep = time.time() + 1
         return self.ids.db_bar.value
